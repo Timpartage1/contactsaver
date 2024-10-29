1aa745b27941a6e3db1971260b41402d266e9a04
Timothee Katende <105564563+Timpartage1@users.noreply.github.com>
2024-10-29 16:35:47 +0100

    Update contact.js db

--- models/contact.js
+++ models/contact.js
@@ -1,6 +1,6 @@
 const { Sequelize, DataTypes } = require('sequelize');
-const sequelize = new Sequelize('contacts', 'root', '', {
-  host: 'localhost',
+const sequelize = new Sequelize('sql12741536', 'sql12741536', 'ks7R4j7Rh9', {
+  host: 'sql12.freesqldatabase.com',
   dialect: 'mysql'
 });
 
